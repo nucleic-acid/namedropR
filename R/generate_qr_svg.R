2e3a79e38e1660b8fe0177237f2140af23f938c8
Christian Gebhard <christian.gebhard@posteo.de>
2022-01-03 14:55:48 +0100

    Adds a tryCatch structure for QR-code generation function.

--- R/generate_qr_svg.R
+++ R/generate_qr_svg.R
@@ -9,14 +9,28 @@
 #'
 #' @examples
 #' \dontrun{
-#' generate_svg("https://doi.org/10.1007/bf00396886", cite_key = "Eschrich1983")
+#' generate_qr_svg("https://doi.org/10.1007/bf00396886", cite_key = "Eschrich1983")
 #' }
 #'
 #' @importFrom qrcode qr_code generate_svg
 
+
 generate_qr_svg <- function(url, cite_key) {
+
+  # CHECK INPUTS
+  stopifnot(class(url) == "character")
+  stopifnot(class(cite_key) == "character")
+
   # create QR-Code
   doi_qr <- qrcode::qr_code(url)
+
   # store QR-code in tempdir()
-  qrcode::generate_svg(doi_qr, filename = paste0(tempdir(),"/qr_codes/qr_",cite_key, ".svg"), show = FALSE)
+  tryCatch(
+    expr = {
+      qrcode::generate_svg(doi_qr, filename = paste0(tempdir(),"/qr_codes/qr_",cite_key, ".svg"), show = FALSE)
+    }
+    error = function(e){
+      warning("Something went wrong creating the QR code for ", cite_key, "! No QR code will be stored to file.")
+      print(e)
+    })
 }
