574a5843097aff2e41b9aef27fcaf28b3ca5ff6d
Christian Gebhard <christian.gebhard@posteo.de>
2022-01-03 08:30:47 +0100

    Add code comments.

--- R/get_css_styles.R
+++ R/get_css_styles.R
@@ -4,17 +4,17 @@
 #' If "none" is given, the returned styles are empty strings.
 #'
 #' @param style A string specifying the desired style for the visual citation.
-
 #' @return A list of inline css styles for each element of the visual citation: top row, title row and author row.
 #'
 #' @examples
 #' modern_style <- get_css_styles(style = "modern")
 #' print(modern_style$title_row_style)
-#'
-
 get_css_styles <- function(style) {
+
+  # initiate empty list
   css_styles <- list()
 
+  # assign inline CSS strings to the respective list items according to the style argument
   if (style == "classic") {
     css_styles$top_row_style <- paste("font-size: 1.3rem;font-family: 'Palatino', 'Georgia', 'Times New Roman', serif;font-weight: normal;font-variant: small-caps;color:#e72e00;")
     css_styles$title_row_style <- paste("font-size: 2rem;font-family: 'Palatino', 'Georgia', 'Times New Roman', serif;font-weight: bold")
@@ -29,5 +29,6 @@ get_css_styles <- function(style) {
     css_styles$author_row_style <- paste("font-size: 1.5rem;font-family: 'Noto Sans', 'Arial', 'Helvetica', sans-serif;font-weight: bold;")
   }
 
+  # return the list
   return(css_styles)
 }
