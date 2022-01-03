7b23aa03648812a33d3b0de526369e6c0cb2354c
Christian Gebhard <christian.gebhard@posteo.de>
2022-01-03 16:46:13 +0100

    Refines unit tests and documentation.

--- tests/testthat/test-drop_html.R
+++ tests/testthat/test-drop_html.R
@@ -1,3 +1,6 @@
 test_that("an HTML taglist is being returned", {
-  # htmltools::
+  expect_equal(class(drop_html(title = "A", journal = "B",
+                         authors = "C", year = "D",
+                         cite_key = "E", url = "F", include_qr = "embed", style = "modern")),
+               class(htmltools::tagList()))
 })
--- tests/testthat/test-generate_qr.R
+++ tests/testthat/test-generate_qr.R
@@ -1,3 +1,4 @@
 test_that("QR code is created and is returned as matrix", {
-  # expect_true(class(generate_qr("https://en.wikipedia.org/")) == "qr_code")
+  expect_equal(class(generate_qr("https://en.wikipedia.org/")),
+               class(qrcode::qr_code("https://en.wikipedia.org/")))
 })
