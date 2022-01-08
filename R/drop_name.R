7920ec44ac4eec1a499c1304b83ffe14d2a3c887
Christian Gebhard <christian.gebhard@posteo.de>
2022-01-08 19:18:09 +0100

    Adds a warning, if any of the  provided cite_keys is not found in the library.
    Also improves documentation and updates the example.

--- R/drop_name.R
+++ R/drop_name.R
@@ -3,15 +3,15 @@
 #' @description Extracts metadata from a .bib file and exports the visual citation in the specified format.
 #'
 #' @param bib Accepts one of the following:
-#' A data.frame or tibble containing the columns YEAR, JOURNAL, AUTHOR, TITLE (all other columns will be dropped) or
-#' a file path to a .bib file.
-#' @param cite_key A string specifying the citation key within the .bib file. If no key is specified, the first entry is used.
-#' @param export_as A string specifying the desired output format. For now supports PNG and HTML by
-#' using "html" to include the 'bare' taglist or "html_full" to write a standalone .html file inlcuding <head> etc.
+#' 1) A data.frame or tibble containing the columns YEAR, JOURNAL, AUTHOR, TITLE, BIBTEXKEY (all mandatory) and DOI, URL (optional).
+#' 2) A file path to a bibliography file in BibTeX/BibLaTeX format (usually *.bib file).
+#' @param cite_key If given, either a character string or a vector of strings are accepted.
+#' Specifies the reference items within the bibliography for which visual citations should be created.
+#' If no key is specified, a visual citation is created for ALL reference items within the bibliography.
+#' @param export_as A string specifying the desired output format. For now supports PNG and HTML.
+#' Use "html" to include the 'bare' taglist (recommended for inclusion in Rmarkdown documents) or "html_full" to write a standalone .html file inlcuding <head> etc.
 #' The PNG is a screenshot of the rendered HTML via the 'webshot' package. The filename represents this two step approach on purpose.
 #' For webshot you need to install phantomJS once (see 'webshot' documentation).
-#' @param inline If TRUE, the output is directly returned. Otherwise the output is stored to disk and a filepath is returned as string.
-#' See also 'path_absolute' parameter.
 #' @param output_dir A string specifying the relative path, where the rendered output files should be stored.
 #' @param max_authors Integer number of maximum authors to print. If the number of authors exceeds this, the list is cropped accordingly.
 #' @param include_qr Character string specifying the way the QR code should be included or if no QR code should be included.
@@ -22,25 +22,29 @@
 #' @param style A string specifying the desired style for the visual citation. Possible values are:
 #' "modern", "classic", "clean", "none". If "none" is given, the returned html can use a custom css file provided by the user.
 #' This custom CSS file must specify styles for <div> classes "top-row", "title-row" and "author-row".
-#' @param substitute_missing Boolean to specify, whether missing data such as title, journal, year or authors should be substituted (TRUE, default) or left empty (FALSE).
-#' @param path_absolute Boolean to specify, whether the returned output path (when inline = FALSE) is a relative path or an absolute path.
+#' @param path_absolute Boolean to specify, whether the returned output path is a relative path or an absolute path.
 #' @param use_xaringan Boolean to specify if an HTML output is intended to be included in an HTML presentation (like e.g. xaringan) or not.
 #' When including the visual citation via htmltools::includeHTML(), the QR code needs to be in a subfolder
 #' relative to the rendered presentation, not relative to the visual citation.
-#' @return A visual citation in the specified output format.
+#' @return A character string with the file path to the created visual citation in the specified output format.
 #'
 #' @examples
-#' \dontrun{
-#' dropped <- drop_name(
-#'   file = system.file("testdata", "sample.bib", package = "namedropR"),
-#'   cite_key = "collaboration_2019_ApJL",
-#'   export_as = "html",
-#'   inline = TRUE,
-#'   max_authors = 3
-#' )
 #'
-#' htmltools::html_print(dropped)
-#' }
+#' # create sample data
+#' bib_tbl <- dplyr::tribble(
+#' ~TITLE, ~AUTHOR, ~JOURNAL, ~BIBTEXKEY, ~YEAR,
+#' "Some title", c("Alice", "Bob", "Charlie"), "Journal of Unnecessary R Packages", "Alice2022", "2022")
+#'
+#' # create visual citation
+#' drop_name(
+#'   bib = bib_tbl,
+#'   cite_key = "Alice2022",
+#'   export_as = "png",
+#'   max_authors = 2,
+#'   style = "clean"
+#'   output_dir = tempdir()
+#'   )
+#'
 #' @export
 #' @import bib2df
 #' @import dplyr
@@ -49,14 +53,12 @@
 #' @importFrom lubridate year ymd
 #' @importFrom webshot webshot
 
-drop_name <- function(bib, cite_key = "collaboration_2019_ApJL",
+drop_name <- function(bib, cite_key,
                       output_dir = "visual_citations",
                       export_as = "html",
-                      inline = FALSE,
                       max_authors = 3,
                       include_qr = "link",
                       style = "modern",
-                      substitute_missing = TRUE,
                       path_absolute = FALSE,
                       use_xaringan = FALSE) {
 
@@ -64,7 +66,6 @@ drop_name <- function(bib, cite_key = "collaboration_2019_ApJL",
   stopifnot(is.character(output_dir))
   stopifnot(is.character(export_as))
   stopifnot(export_as %in% c("html", "html_full", "png"))
-  stopifnot(is.logical(inline))
   stopifnot(is.numeric(max_authors))
   stopifnot(max_authors >= 0)
   stopifnot(is.character(include_qr))
@@ -215,6 +216,8 @@ drop_name <- function(bib, cite_key = "collaboration_2019_ApJL",
   } else {
     n_bib <- length(cite_key)
     message(paste0(n_bib, " cite_key(s) specified. Working through all of them."))
+    missing_keys <- cite_key[!(cite_key %in% work_list$BIBTEXKEY)]
+    warning(paste0("The following cite_key items were not found in the provided library: ", missing_keys))
     work_list <- clean_bib %>%
       filter(BIBTEXKEY %in% cite_key)
   }
@@ -231,7 +234,7 @@ drop_name <- function(bib, cite_key = "collaboration_2019_ApJL",
   vcs = apply(
     work_list,
     1,
-    drop_html2,
+    drop_html,
     include_qr = include_qr,
     style = style,
     output_dir = output_dir,
@@ -246,15 +249,11 @@ drop_name <- function(bib, cite_key = "collaboration_2019_ApJL",
       vcs = vcs
     )
 
-  if (inline) { # return the dataframe with the html tag list as result
-    return(work_list)
-  } else {
-    file_paths <- apply(work_list, 1, FUN = write_vc,
+  file_paths <- apply(work_list, 1, FUN = write_vc,
           output_dir = output_dir,
           path_absolute = path_absolute,
           export_as = export_as
           )
   return(file_paths)
-  }
 
 }
