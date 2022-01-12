f8f3819e9dfe5ae8cdaf308997349383b6c62436
Christian Gebhard <christian.gebhard@posteo.de>
2022-01-12 22:38:16 +0100

    Removes reduntant or obsolete testing code, adds further testing for new dependency code.

--- tests/testthat/test-drop_name.R
+++ tests/testthat/test-drop_name.R
@@ -112,91 +112,46 @@ test_that("bulk operations work properly", {
 
   expect_warning(drop_name(bib = bulk_data_dup, cite_key = "Alice2021", output_dir = tempdir()), "BIBTEX keys are not unique")
 })
-#
-# test_that("output_dir is properly checked", {
-#   bib_path <- system.file("testdata", "sample.bib", package = "namedropR")
-#   test_bibfile <- suppressMessages(RefManageR::ReadBib(bib_path))
-#   expect_error(drop_name(bib = test_bibfile, output_dir = TRUE))
-#   expect_error(drop_name(bib = test_bibfile, output_dir = 5))
-# })
-#
-# test_that("export_as is properly checked", {
-#   bib_path <- system.file("testdata", "sample.bib", package = "namedropR")
-#   test_bibfile <- suppressMessages(RefManageR::ReadBib(bib_path))
-#   expect_error(drop_name(bib = test_bibfile, export_as = "no export option"))
-#   expect_error(drop_name(bib = test_bibfile, cite_key = 5))
-# })
-#
-# test_that("max_authors is properly checked", {
-#   bib_path <- system.file("testdata", "sample.bib", package = "namedropR")
-#   test_bibfile <- suppressMessages(RefManageR::ReadBib(bib_path))
-#   expect_error(drop_name(bib = test_bibfile, max_authors = "many authors"))
-#   expect_error(drop_name(bib = test_bibfile, max_authors = -1))
-# })
-#
-# test_that("inline is properly checked", {
-#   bib_path <- system.file("testdata", "sample.bib", package = "namedropR")
-#   test_bibfile <- suppressMessages(RefManageR::ReadBib(bib_path))
-#   expect_error(drop_name(bib = test_bibfile, inline = "5"))
-# })
-#
-# test_that("inline = TRUE returns a non-empty tagList", {
-#   bib_path <- system.file("testdata", "sample.bib", package = "namedropR")
-#   test_bibfile <- suppressMessages(RefManageR::ReadBib(bib_path))
-#   expect_true(any(class(drop_name(bib = test_bibfile, cite_key = "Eschrich1983", export_as = "html", inline = TRUE)) == "shiny.tag.list"))
-#   expect_true(length(drop_name(bib = test_bibfile, cite_key = "Eschrich1983", export_as = "html", inline = TRUE)) > 0)
-# })
-#
-# test_that("inline = FALSE returns a correct file path as character", {
-#   bib_path <- system.file("testdata", "sample.bib", package = "namedropR")
-#   test_bibfile <- suppressMessages(RefManageR::ReadBib(bib_path))
-#   expect_type(drop_name(bib = test_bibfile, cite_key = "Eschrich1983", inline = FALSE), "character")
-#   expect_true(file.exists(here::here(drop_name(bib = test_bibfile, cite_key = "Eschrich1983", inline = FALSE))))
-# })
-#
-# test_that("include_qr is properly checked", {
-#   bib_path <- system.file("testdata", "sample.bib", package = "namedropR")
-#   test_bibfile <- suppressMessages(RefManageR::ReadBib(bib_path))
-#   expect_error(drop_name(bib = test_bibfile, include_qr = "invalid option"))
-#   expect_error(drop_name(bib = test_bibfile, include_qr = 5))
-# })
-#
-# test_that("style is properly checked", {
-#   bib_path <- system.file("testdata", "sample.bib", package = "namedropR")
-#   test_bibfile <- suppressMessages(RefManageR::ReadBib(bib_path))
-#   expect_error(drop_name(bib = test_bibfile, style = 987))
-# })
-#
-# test_that("substitute_missing is properly checked", {
-#   bib_path <- system.file("testdata", "sample.bib", package = "namedropR")
-#   test_bibfile <- suppressMessages(RefManageR::ReadBib(bib_path))
-#   expect_error(drop_name(bib = test_bibfile, substitute_missing = 987))
-# })
-#
-# test_that("use_xaringan is properly checked", {
-#   bib_path <- system.file("testdata", "sample.bib", package = "namedropR")
-#   test_bibfile <- suppressMessages(RefManageR::ReadBib(bib_path))
-#   expect_error(drop_name(bib = test_bibfile, use_xaringan = 987))
-#   unlink(here::here("visual_citations"), recursive = TRUE)
-# })
-#
-#
-# # TECHNICAL OUTPUT CHECKS
-# test_that("use_xaringan creates the correct folders (qr/ in working dir)", {
-#   bib_path <- system.file("testdata", "sample.bib", package = "namedropR")
-#   test_bibfile <- suppressMessages(RefManageR::ReadBib(bib_path))
-#
-#   drop_name(bib = test_bibfile, export_as = "html", cite_key = "collaboration_2019_ApJL", use_xaringan = TRUE)
-#   expect_true(dir.exists(here::here("visual_citations")))
-#   expect_true(dir.exists(here::here("qr")))
-#
-#   unlink(here::here("visual_citations"), recursive = TRUE)
-#   unlink(here::here("qr"), recursive = TRUE)
-#
-#   drop_name(bib = test_bibfile, export_as = "html_full", cite_key = "collaboration_2019_ApJL", use_xaringan = TRUE)
-#   expect_true(dir.exists(here::here("visual_citations")))
-#   expect_true(dir.exists(here::here("qr")))
-#
-#   unlink(here::here("visual_citations"), recursive = TRUE)
-#   unlink(here::here("qr"), recursive = TRUE)
-# })
+
+test_that("output_dir is properly checked and/or created", {
+  sample_data <- dplyr::tribble(
+    ~TITLE, ~AUTHOR, ~JOURNAL, ~BIBTEXKEY, ~YEAR, ~DOI,
+    "Title1", c("Alice1", "Bob1", "Charlie1"), "JoURP1", "Alice2021", "2021", "someDOI1",
+  )
+
+  # test wrong input formats
+  expect_error(drop_name(bib = sample_data, output_dir = TRUE))
+  expect_error(drop_name(bib = sample_data, output_dir = 5))
+})
+
+test_that("use_xaringan is properly checked", {
+  sample_data <- dplyr::tribble(
+    ~TITLE, ~AUTHOR, ~JOURNAL, ~BIBTEXKEY, ~YEAR, ~DOI,
+    "Title1", c("Alice1", "Bob1", "Charlie1"), "JoURP1", "Alice2021", "2021", "someDOI1",
+  )
+  expect_error(drop_name(bib = sample_data, use_xaringan = 987))
+})
+
+test_that("use_xaringan creates the correct folders (qr/ in working dir)", {
+  sample_data <- dplyr::tribble(
+    ~TITLE, ~AUTHOR, ~JOURNAL, ~BIBTEXKEY, ~YEAR, ~DOI,
+    "Title1", c("Alice1", "Bob1", "Charlie1"), "JoURP1", "Alice2021", "2021", "someDOI1",
+  )
+  # remove possibly preexisting folders
+  unlink(here::here("visual_citations"), recursive = TRUE)
+  unlink(here::here("qr"), recursive = TRUE)
+
+  drop_name(bib = sample_data, export_as = "html", cite_key = "Alice2021", use_xaringan = TRUE)
+  expect_true(dir.exists(here::here("visual_citations")))
+  expect_true(dir.exists(here::here("qr")))
+
+  unlink(here::here("visual_citations"), recursive = TRUE)
+  unlink(here::here("qr"), recursive = TRUE)
+
+  drop_name(bib = sample_data, export_as = "html_full", cite_key = "Alice2021", use_xaringan = TRUE)
+  expect_true(dir.exists(here::here("visual_citations")))
+  expect_true(dir.exists(here::here("qr")))
+
+  unlink(here::here("visual_citations"), recursive = TRUE)
+  unlink(here::here("qr"), recursive = TRUE)
+})
