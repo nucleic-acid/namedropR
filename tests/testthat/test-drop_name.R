224ee075e94aee31b17a57a41989c86a4e918761
Christian Gebhard <christian.gebhard@posteo.de>
2022-05-15 19:19:53 +0200

    Adds withr handling to some tests.

--- tests/testthat/test-drop_name.R
+++ tests/testthat/test-drop_name.R
@@ -1,70 +1,114 @@
 # CHECK INPUTS
 test_that("Bibliographies are correctly read ", {
+  new <- tempdir()
+
   empty_bib <- dplyr::tribble(
     ~TITLE, ~AUTHOR, ~JOURNAL, ~BIBTEXKEY, ~YEAR, ~DOI
   )
-  expect_error(drop_name(bib = empty_bib, cite_key = "Eschrich1983"), "is empty")
+  withr::with_dir(
+    new = new,
+    code = {
+      expect_error(drop_name(bib = empty_bib, cite_key = "Eschrich1983"), "is empty")
+    }
+  )
 
   bib_path <- system.file("testdata", "sample.bib", package = "namedropR")
-  expect_error(drop_name(bib = "wrong_file_path", cite_key = "Eschrich1983"), "file not found")
-  expect_error(drop_name(bib = 123), "Inappropriate type of bibliography")
-  expect_error(drop_name(cite_key = "Eschrich1983"), "No bibliography")
+  withr::with_dir(
+    new = new,
+    code = {
+      expect_error(drop_name(bib = "wrong_file_path", cite_key = "Eschrich1983"), "file not found")
+      expect_error(drop_name(bib = 123), "Inappropriate type of bibliography")
+      expect_error(drop_name(cite_key = "Eschrich1983"), "No bibliography")
+    }
+  )
 
   good_bib <- system.file("testdata", "good.bib", package = "namedropR")
-  expect_message(
-    expect_message(
-      drop_name(bib = good_bib, cite_key = "Eschrich1983"),
-      "Years coerced to string format."
-    ),
-    "Bibliography file successfully read."
+  withr::with_dir(
+    new = new,
+    code = {
+      expect_message(
+        expect_message(
+          drop_name(bib = good_bib, cite_key = "Eschrich1983"),
+          "Years coerced to string format."
+        ),
+        "Bibliography file successfully read."
+      )
+    }
   )
 })
 
 test_that("compact mode returns a correct file path", {
+  new <- tempdir()
   slim_bib <- dplyr::tribble(
     ~TITLE, ~AUTHOR, ~JOURNAL, ~BIBTEXKEY, ~DATE,
     "Some 2022", c("Alice", "Bob", "Charlie"), "Journal of Unnecessary R Packages", "Alice2022", "2022"
   )
 
-  expect_equal(
-    drop_name(bib = slim_bib, cite_key = "Alice2022", output_dir = tempdir(), style = "compact"),
-    normalizePath(file.path(tempdir(), "Alice2022.html"))
+  withr::with_dir(
+    new = new,
+    code = {
+      expect_equal(
+        drop_name(bib = slim_bib, cite_key = "Alice2022", output_dir = "drop_name01", style = "compact"),
+        file.path("drop_name01", "Alice2022.html")
+      )
+    }
   )
 })
 
 test_that("biblatex files produce an output", {
+  new <- tempdir()
+
   bib_tbl <- dplyr::tribble(
     ~TITLE, ~AUTHOR, ~JOURNALTITLE, ~BIBTEXKEY, ~DATE, ~URL,
     "Some 2022", c("Alice", "Bob", "Charlie"), "Journal of Unnecessary R Packages", "Alice2022", "2022", "https://en.wikipedia.org"
   )
-  expect_equal(
-    drop_name(bib = bib_tbl, cite_key = "Alice2022", output_dir = tempdir()),
-    file.path(tempdir(), "Alice2022.html")
+
+  withr::with_dir(
+    new = new,
+    code = {
+      expect_equal(
+        drop_name(bib = bib_tbl, cite_key = "Alice2022", output_dir = "drop_name02"),
+        file.path("drop_name02", "Alice2022.html")
+      )
+    }
   )
+
 
   bib_tbl2 <- dplyr::tribble(
     ~TITLE, ~AUTHOR, ~JOURNALTITLE, ~BIBTEXKEY, ~DATE, ~YEAR, ~URL,
     "Some 2022", c("Alice", "Bob", "Charlie"), "Journal of Unnecessary R Packages", "Alice2022", "2022-01-01", NA, "https://en.wikipedia.org",
     "Some 2023", c("Alice", "Bob", "Charlie"), "Journal of Unnecessary R Packages", "Alice2023", NA, "2023", "https://en.wikipedia.org"
   )
-  expect_equal(
-    suppressMessages(drop_name(bib = bib_tbl2, output_dir = tempdir())),
-    c(
-      file.path(tempdir(), "Alice2022.html"),
-      file.path(tempdir(), "Alice2023.html")
-    )
+
+  withr::with_dir(
+    new = new,
+    code = {
+      expect_equal(
+        suppressMessages(drop_name(bib = bib_tbl2, output_dir = "drop_name03")),
+        c(
+          file.path("drop_name03", "Alice2022.html"),
+          file.path("drop_name03", "Alice2023.html")
+        )
+      )
+    }
   )
 })
 
 test_that("missing DOI and URL columns are properly handled", {
+  new <- tempdir()
   slim_bib <- dplyr::tribble(
     ~TITLE, ~AUTHOR, ~JOURNAL, ~BIBTEXKEY, ~DATE,
     "Some 2022", c("Alice", "Bob", "Charlie"), "Journal of Unnecessary R Packages", "Alice2022", "2022"
   )
 
-  expect_equal(
-    drop_name(bib = slim_bib, cite_key = "Alice2022", output_dir = tempdir()),
-    file.path(tempdir(), "Alice2022.html")
+  withr::with_dir(
+    new = new,
+    code = {
+      expect_equal(
+        drop_name(bib = slim_bib, cite_key = "Alice2022", output_dir = "drop_name04"),
+        file.path("drop_name04", "Alice2022.html")
+      )
+    }
   )
 })
 
@@ -120,7 +164,7 @@ test_that("bulk operations work properly", {
   expect_warning(drop_name(bib = bulk_data_dup, cite_key = "Alice2021", output_dir = tempdir()), "BIBTEX keys are not unique")
 })
 
-test_that("output_dir is properly checked and/or created", {
+test_that("output_dir is properly checked", {
   sample_data <- dplyr::tribble(
     ~TITLE, ~AUTHOR, ~JOURNAL, ~BIBTEXKEY, ~YEAR, ~DOI,
     "Title1", c("Alice1", "Bob1", "Charlie1"), "JoURP1", "Alice2021", "2021", "someDOI1",
