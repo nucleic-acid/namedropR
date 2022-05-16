39805708a82c0553c340af929c3bc2302cbab68d
Christian Gebhard <christian.gebhard@posteo.de>
2022-05-16 21:11:07 +0200

    Implements R.utils file path determination as well png option.

--- R/write_vc.R
+++ R/write_vc.R
@@ -91,6 +91,7 @@ write_vc <- function(work_item, path_absolute, output_dir, export_as) {
         }
       )
 
+
       tryCatch(
         expr = {
           webshot::webshot(output_file, paste0(output_file, ".png"), selector = ".visual-citation", zoom = 2)
@@ -105,23 +106,15 @@ write_vc <- function(work_item, path_absolute, output_dir, export_as) {
         }
       )
       unlink(output_file)
+
       # to point to the png instead return its filepath
+      png_out <- paste0(output_file, ".png")
 
       return_path_png <- ifelse(
           path_absolute,
-          paste0(output_file, ".png"),
-          file.path(
-            output_dir,
-            paste0(
-             sub(
-              pattern = "^[\\/]",
-              replacement = "",
-              sub(normalizePath(output_dir), replacement = "", output_file)
-            ),
-            ".png"
-            )
+          R.utils::getAbsolutePath(png_out),
+          R.utils::getRelativePath(png_out)
           )
-        )
       return(return_path_png)
     }
   } else {
