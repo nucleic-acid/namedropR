27aee9b820f6c30975ecd7b056a21e1c3982f83c
Lukas Wallrich <lukas.wallrich@gmail.com>
2022-05-20 18:08:16 +0100

    Allow user to specify keys (by naming DOI vector)

--- R/drop_name_crossref.R
+++ R/drop_name_crossref.R
@@ -2,14 +2,17 @@
 #'
 #' Takes one or several dois and extracts information from Crossref, then processes them into visual citations with \code{\link{drop_name}}
 #'
-#' @param dois One or several dois to create visual citations for
+#' @param dois One or several dois to create visual citations for. If they are named, these names are used as filenames; otherwise they are generated based on authors and years.
 #' @inheritDotParams drop_name -bib -cite_key
 #'
 #' @export
 
 drop_name_crossref <- function(dois, ...) {
 
-  if (!require(rcrossref)) stop("This function requires the rcrossref package. Please install and retry.")
+  if (!"rcrossref" %in% rownames(installed.packages())) {
+    stop("This function requires the rcrossref package. Please install and retry.")
+  }
+
   stopifnot(is.character(dois))
 
   ref <-  rcrossref::cr_cn(dois, "citeproc-json")
@@ -23,9 +26,16 @@ drop_name_crossref <- function(dois, ...) {
 
   df <- dplyr::bind_rows(lapply(ref, to_df))
 
+  #Use names of DOIs as keys where provided
+  if(!is.null(names(dois))) {
+    df$BIBTEXKEY[stringr::str_length(names(dois))>0] <- names(dois)[stringr::str_length(names(dois))>0]
+  }
+
   #Ensure BIBTEXKEY are unique
   letters_blank <- c("", letters)
   df <- dplyr::ungroup(dplyr::mutate(dplyr::group_by(df, BIBTEXKEY), BIBTEXKEY = paste0(BIBTEXKEY, letters_blank[1:dplyr::n()])))
+
+
 
   drop_name(df, ...)
 
