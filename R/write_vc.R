24f151a649955060b2f26326d7c0959a9952bcfb
Lukas Wallrich <lukas.wallrich@gmail.com>
2022-12-11 13:08:27 +0000

    Change to webshot2 and remove phantomjs check

--- R/write_vc.R
+++ R/write_vc.R
@@ -73,10 +73,7 @@ write_vc <- function(work_item, path_absolute, output_dir, export_as) {
       }
     )
   } else if (export_as == "png") {
-    if (!webshot::is_phantomjs_installed()) {
-      message("You need to download and install phantomJS to save output as PNG. Try running 'webshot::install_phantomjs()' once.")
-    } else {
-      # renders as "complete" html to get the white background for PNG snapshot.
+       # renders as "complete" html to get the white background for PNG snapshot.
       tryCatch(
         expr = {
           htmltools::save_html(work_item$vcs, file = output_file)
@@ -89,12 +86,10 @@ write_vc <- function(work_item, path_absolute, output_dir, export_as) {
           message("Having difficulties saving the intermediate HTML output:")
           print(w)
         }
-      )
-
 
       tryCatch(
         expr = {
-          webshot::webshot(output_file, paste0(output_file, ".png"), selector = ".visual-citation", zoom = 2)
+          webshot2::webshot(output_file, paste0(output_file, ".png"), selector = ".visual-citation", zoom = 2)
         },
         error = function(e) {
           message("Could not take a screenshot of the intermediate HTML.")
