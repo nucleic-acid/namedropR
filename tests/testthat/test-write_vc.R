efdf24548abfda6115f332e4a933136a5b00b8a3
Lukas Wallrich <lukas.wallrich@gmail.com>
2022-12-11 14:12:57 +0000

    Change to webshot2

--- tests/testthat/test-write_vc.R
+++ tests/testthat/test-write_vc.R
@@ -142,8 +142,12 @@ test_that("saving as html works as expected", {
 #     "Some 2023", "Alice; Bob; Charlie", "Journal of Unnecessary R Packages", "Alice2023", "2023", VCS
 #   )
 #
-#   # writes a file, this test can only be run, if phantomJS is installed:
-#   if(webshot::is_phantomjs_installed()) {
+<<<<<<< HEAD
+#   # writes a file, this test can only be run, if a chromium browser is installed:
+=======
+#   # writes a file, this test can only be run, if a chromium browser is installed
+>>>>>>> 7cf2526520982023deee1feb342faca1d1023a08
+#   if(!is.null(chromote::find_chrome())) {
 #     withr::with_dir(
 #       new = new,
 #       code = {
